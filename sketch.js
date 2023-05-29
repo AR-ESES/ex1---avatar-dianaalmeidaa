fe1467cac008ce305e9ee9e5465f133532d9411f
dianaalmeidaa <diana.p.almeida1@gmail.com>
2023-05-29 17:23:40 +0100

    Código

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+
+	background(255);
+	noFill();
+	stroke(50,255);
+	circle(width /2, height /2, height / 4,);
+	circle(width /2 - height /5/4, height /2 - height/18, height /12 );
+	circle(width /2 + height /5/4, height /2 - height/18, height /12 );
+	ellipse(645,475,280,242);
+	ellipse(850,410,170,52);
+	ellipse(441,410,170,52);
+
+}
